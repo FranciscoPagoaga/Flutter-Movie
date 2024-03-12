79a55e38aafcadc866cb791fbc5a205af67b121e
Francisco Pagoaga <50030247+FranciscoPagoaga@users.noreply.github.com>
2024-03-12 11:37:12 -0600

    created routes and screens for home, signin and splash

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,10 +1,63 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+import '../../../../../main.dart';
+import '../../../routes/routes.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        _init();
+      },
+    );
+  }
+
+  Future<void> _init() async {
+    final injector = Injector.of(context);
+    final connectivityRepository = injector.connectivityRepository;
+    final hasInternet = await connectivityRepository.hasInternet;
+
+    if (hasInternet) {
+      final authenticationRepository = injector.authenticationRepository;
+      final isSignedIn = await authenticationRepository.isSignedIn;
+      if (isSignedIn) {
+        final user = await authenticationRepository.getUserData();
+        if (mounted) {
+          if (user != null) {
+            _goTo(Routes.home);
+          } else {
+            _goTo(Routes.signIn);
+          }
+        }
+      } else if (mounted) {
+        _goTo(Routes.signIn);
+      }
+    } else {}
+  }
+
+  void _goTo(String routeName) {
+    Navigator.pushReplacementNamed(context, routeName);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 80,
+          width: 80,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
   }
 }
--- lib/app/presentation/routes/app_routes.dart
+++ lib/app/presentation/routes/app_routes.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/widgets.dart';
+
+import '../modules/home/views/home_view.dart';
+import '../modules/sign_in/views/sign_in.dart';
 import '../modules/splash/views/splash_view.dart';
 import 'routes.dart';
 
-Map<String, Widget Function(BuildContext)> get appRoutes{
+Map<String, Widget Function(BuildContext)> get appRoutes {
   return {
     Routes.splash: (context) => const SplashView(),
+    Routes.signIn: (context) => const SignInView(),
+    Routes.home: (context) => const HomeView(),
   };
 }
